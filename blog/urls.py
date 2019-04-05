e7acfb36e99a9ebb65a2bd79030f2e033caa2d92
Meisy Tunay <mtunay@Meisys-MacBook-Air.local>
2019-04-05 02:27:33 -0700

    fiusing version control

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+'''
+urls module for views
+'''
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+]
